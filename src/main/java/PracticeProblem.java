93460267833d95c53458363cd33c0c5aebe96861
jathav-puvirajan-ics4u1-03-25 <jathav.puvirajan@student.tdsb.on.ca>
2025-02-21 18:06:16 +0000

    b

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,5 +3,41 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static int lettersToFive(String str, char chr){
+		int c = 0;
+		int i = 0;
+		while(i < str.length()){
+			if(str.charAt(i) == chr){
+				c++;
+			}
+			i++;
+			if(c==5){
+				return i;
+			}
+		}
+		return -1;
+	}
+	public static int countLetter(String str, char chr){
+		int c = 0;
+		int i = 0;
+		while(i < str.length()){
+			if(str.charAt(i) == chr){
+				c++;
+			}
+			i++;
+		}
+		return c;
+	}
+	public static int oddSum(int num1, int num2){
+		int sum = 0;
+		for(int i = Math.min(num1,num2);i<=Math.max(num1,num2);i++){
+			if(i%2==0) continue;
+			sum +=i;
+		}
+		return sum;
+	}
+
+	
+
 
 }
